3f75a64c836cb65319ddc4a1c7b8497fdd847cf1
Harry Patidar <patidar.harry@gmail.com>
2018-01-12 17:40:41 +0530

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/harry/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.harry.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
